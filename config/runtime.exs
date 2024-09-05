d93f7a51e3a8205ff5223b0dcb605412239496e0
Philip Chani <chaniphilip@hotmail.com>
2024-09-05 21:35:24 +0200

    Added database configs directly (wrong)

--- config/runtime.exs
+++ config/runtime.exs
@@ -16,17 +16,12 @@ import Config
 #
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
-if System.get_env("PHX_SERVER") do
-  config :mockup_bank, MockupBankWeb.Endpoint, server: true
-end
+# if System.get_env("PHX_SERVER") do
+config :mockup_bank, MockupBankWeb.Endpoint, server: true
+# end
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
+  database_url = "ecto://postgres:Qwerty12@srv-captain--postgres/mockupserver"
 
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
@@ -41,12 +36,7 @@ if config_env() == :prod do
   # want to use a different value for prod and you most likely don't want
   # to check this value into version control, so we use an environment
   # variable instead.
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+  secret_key_base = "2lfGLtwPwlXo4koQZTSPtoaovMmNT1g4uXDF+OYedtu+h4X3p+3zjNHQz3/gxpFu"
 
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
