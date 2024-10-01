21c5f7ea260873900ca5c858a1fc0768dedf06d7
Philip Chani <chaniphilip@hotmail.com>
2024-10-01 19:46:14 +0200

    added fields

--- lib/mockup_bank/database/user_accounts_schema.ex
+++ lib/mockup_bank/database/user_accounts_schema.ex
@@ -10,14 +10,31 @@ defmodule MockupBank.Database.UserAccounts do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @derive {Jason.Encoder, only: [:account_number, :balance, :currency, :account_type, :status, :last_transaction_date]}
+  @derive {Jason.Encoder, only: [:nickname, :account_number, :balance, :currency, :account_type, :status, :last_transaction_date, :hidden, :frozen, :enable_alerts, :low_balance_alert, :low_balance_threshold, :large_transaction_alert, :large_transaction_threshold, :suspicous_activity_alert, :suspicous_activity_seconds_between_transactions]}
   schema "user_accounts" do
+    field :nickname, :string
     field :account_number, :string
     field :balance, :decimal
     field :currency, :string
     field :account_type, :string
     field :status, :string
     field :last_transaction_date, :naive_datetime
+
+
+    field :hidden, :boolean, default: false
+    field :frozen, :boolean, default: false
+    field :enable_alerts, :boolean, default: true
+
+    field :low_balance_alert, :boolean, default: false
+    field :low_balance_threshold, :decimal, default: 0.0
+
+    field :large_transaction_alert, :boolean, default: false
+    field :large_transaction_threshold, :decimal, default: 0.0
+
+    field :suspicous_activity_alert, :boolean, default: false
+    field :suspicous_activity_seconds_between_transactions, :integer, default: 0
+
+
     belongs_to :account_users, MockupBank.Database.AccountUsers
 
     timestamps()
@@ -25,7 +42,7 @@ defmodule MockupBank.Database.UserAccounts do
 
   def changeset(user_account, attrs) do
     user_account
-    |> cast(attrs, [:account_users_id, :account_number, :balance, :currency, :account_type, :status, :last_transaction_date])
+    |> cast(attrs, [:nickname, :account_users_id, :account_number, :balance, :currency, :account_type, :status, :last_transaction_date])
     |> validate_required([:account_number, :balance, :currency, :account_type, :status])
     |> unique_constraint(:account_number)
   end
