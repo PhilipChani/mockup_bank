fb74236864837403215f3eaa991ff40ec08d2ccd
Philip Chani <chaniphilip@hotmail.com>
2024-10-01 19:42:23 +0200

    added more fields

--- priv/repo/migrations/20240908180247_account_users.exs
+++ priv/repo/migrations/20240908180247_account_users.exs
@@ -6,6 +6,7 @@ defmodule MockupBank.Repo.Migrations.AccountUsers do
     create table(:account_users) do
       add :email, :string, null: false
       add :name, :string, null: false
+      add :customer_number, :string
       add :first_name, :string
       add :last_name, :string
       add :phone, :string
@@ -22,12 +23,17 @@ defmodule MockupBank.Repo.Migrations.AccountUsers do
       add :currency, :string
       add :role, :string, null: false
 
+      add :notify_sms, :boolean, default: true
+      add :notify_email, :boolean, default: true
+      add :push_notifications, :boolean, default: true
+
       timestamps()
     end
 
     create unique_index(:account_users, [:email])
 
     create table(:user_accounts) do
+      add :nickname, :string
       add :account_number, :string, null: false
       add :balance, :decimal, null: false, precision: 10, scale: 2
       add :currency, :string, null: false
@@ -35,7 +41,22 @@ defmodule MockupBank.Repo.Migrations.AccountUsers do
       add :status, :string, null: false
       add :last_transaction_date, :naive_datetime
       add :account_users_id, references(:account_users, on_delete: :delete_all), null: false
-
+      add :hidden, :boolean, default: false
+      add :frozen, :boolean, default: false
+      add :enable_alerts, :boolean, default: true
+      add :low_balance_alert, :boolean, default: false
+      add :low_balance_threshold, :decimal, default: 0.0, precision: 10, scale: 2
+      add :large_transaction_alert, :boolean, default: false
+      add :large_transaction_threshold, :decimal, default: 0.0, precision: 10, scale: 2
+      add :suspicous_activity_alert, :boolean, default: false
+      add :suspicous_activity_seconds_between_transactions, :integer, default: 0
+      add :account_creation_date, :naive_datetime
+      add :account_closure_date, :naive_datetime
+      add :account_closure_reason, :string
+      add :joint_account, :boolean, default: false
+      add :joint_account_holder_id, references(:account_users, on_delete: :delete_all)
+      add :overdraft_limit, :decimal, default: 0.0, precision: 10, scale: 2
+      add :overdraft_protection, :boolean, default: false
       timestamps()
     end
 
